99dc950acd866fb81f2272d0f91fef0d176852f5
BernardHugo <115211209+BernardHugo@users.noreply.github.com>
2022-12-15 21:10:38 +0700

    Delete GSLC OOP session 9  2540124450 - Bernard Hugo directory

--- GSLC OOP session 9  2540124450 - Bernard Hugo/src/learning/AirConditioner.java
+++
@@ -1,7 +0,0 @@
-package learning;
-
-// Interface untuk mengatur suhu AC kendaraan dan mengubah mode AC nya
-public interface AirConditioner{
-	public void setTemp();
-	public void changeMod(int mod);
-}
