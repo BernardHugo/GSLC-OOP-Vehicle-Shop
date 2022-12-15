25e65cfda6d7162384939e11ade346344d134bd6
BernardHugo <115211209+BernardHugo@users.noreply.github.com>
2022-12-15 21:04:30 +0700

    Add files via upload

---
+++ GSLC OOP session 9  2540124450 - Bernard Hugo/src/learning/Wing.java
@@ -0,0 +1,7 @@
+package learning;
+
+// Interface untuk mengubah ketinggian pesawat dan mempertahankan ketinggiannya
+public interface Wing{
+	public void changeAlt(int feet);
+	public void rettain();
+}
---
+++ GSLC OOP session 9  2540124450 - Bernard Hugo/src/vehicles/Vehicle.java
@@ -0,0 +1,66 @@
+package vehicles;
+
+public abstract class Vehicle {
+	private String name, fuelType;
+	private int EnginePower, year, totalLamp, speed, price;
+	
+	// Abstract method yang akan digunakan untuk menampilkan data-data dari properti kendaraan yang sudah di isi
+	public abstract String toString();
+	
+	// Abstract method yang akan digunakan untuk menampilkan total harga akhir dari sebuah kendaraan
+	public abstract void calcTotalPrice();
+	
+	public String getFuelType() {
+		return fuelType;
+	}
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+	public int getEnginePower() {
+		return EnginePower;
+	}
+	public void setEnginePower(int enginePower) {
+		EnginePower = enginePower;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getTotalLamp() {
+		return totalLamp;
+	}
+	public void setTotalLamp(int totalLamp) {
+		this.totalLamp = totalLamp;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Vehicle(String name, String fuelType, int enginePower, int year, int totalLamp,
+			int speed, int price) {
+		super();
+		this.name = name;
+		this.fuelType = fuelType;
+		EnginePower = enginePower;
+		this.year = year;
+		this.totalLamp = totalLamp;
+		this.speed = speed;
+		this.price = price;
+	}
+}
