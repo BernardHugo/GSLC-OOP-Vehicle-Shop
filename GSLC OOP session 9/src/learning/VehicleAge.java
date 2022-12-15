fa7c730df9a3baf92e6a37e04e9e2dcc1e7bf01a
BernardHugo <115211209+BernardHugo@users.noreply.github.com>
2022-12-15 15:00:56 +0700

    Delete GSLC OOP session 9 directory

--- GSLC OOP session 9/src/learning/VehicleAge.java
+++
@@ -1,6 +0,0 @@
-package learning;
-
-// Interface untuk mengetahui kendaraan tersebut baru atau tua, sesuai dengan tahun dibuatnya
-public interface VehicleAge {
-	public void newOrOld(int year);
-}
--- GSLC OOP session 9/src/learning/Wing.java
+++
@@ -1,7 +0,0 @@
-package learning;
-
-// Interface untuk mengubah ketinggian pesawat dan mempertahankan ketinggiannya
-public interface Wing{
-	public void changeAlt(int feet);
-	public void rettain();
-}
