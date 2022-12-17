c1168cae7243c5cbd4fc792a0df7016bdae7b523
BernardHugo <115211209+BernardHugo@users.noreply.github.com>
2022-12-17 15:36:52 +0700

    Delete GSLC OOP session 9  2540124450 - Bernard Hugo directory

--- GSLC OOP session 9  2540124450 - Bernard Hugo/src/learning/Steer.java
+++
@@ -1,7 +0,0 @@
-package learning;
-
-// Interface untuk memberikan lampu sen dan memutar roda pengemudi kendaraan
-public interface Steer{
-	public void turn(String direction, int degree);
-	public void lampSign(int type);
-}
